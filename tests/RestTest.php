532965e5190851b959ca0181f02bb2936b9d3589
alphayax <alphayax@gmail.com>
2016-06-07 21:00:44 +0200

    Fix tests

--- tests/RestTest.php
+++ tests/RestTest.php
@@ -7,54 +7,54 @@ class RestTest extends PHPUnit_Framework_TestCase {
 
     public function testConstruct() {
         $rest = new \alphayax\utils\Rest( 'https://api.github.com/users/alphayax/repos');
-        $this->assertAttributeEquals( true, '_isJson', $rest);
-        $this->assertAttributeEquals( true, '_returnAsArray', $rest);
+        $this->assertAttributeEquals( true, 'isJson', $rest);
+        $this->assertAttributeEquals( true, 'returnAsArray', $rest);
     }
 
     public function testHeader() {
         $rest = new \alphayax\utils\Rest( 'https://api.github.com/users/alphayax/repos');
-        $this->assertAttributeEmpty( 'http_headers', $rest);
+        $this->assertAttributeEmpty( 'httpHeaders', $rest);
         $rest->addHeader( 'User-Agent', 'alphayax-php_utils');
-        $this->assertAttributeNotEmpty( 'http_headers', $rest);
-        $this->assertAttributeEquals( ['User-Agent'=> 'alphayax-php_utils'], 'http_headers', $rest);
-        $this->assertAttributeCount(1, 'http_headers', $rest);
+        $this->assertAttributeNotEmpty( 'httpHeaders', $rest);
+        $this->assertAttributeEquals( ['User-Agent'=> 'alphayax-php_utils'], 'httpHeaders', $rest);
+        $this->assertAttributeCount(1, 'httpHeaders', $rest);
         $rest->setContentType_MultipartFormData();
-        $this->assertAttributeCount(2, 'http_headers', $rest);
+        $this->assertAttributeCount(2, 'httpHeaders', $rest);
         $rest->setContentType_XFormURLEncoded();
-        $this->assertAttributeCount(2, 'http_headers', $rest);
+        $this->assertAttributeCount(2, 'httpHeaders', $rest);
     }
 
     public function testGET() {
         $rest = new \alphayax\utils\Rest( 'http://jsonplaceholder.typicode.com/posts/1');
         $rest->addHeader( 'User-Agent', 'alphayax-php_utils');
-        $this->assertAttributeEmpty( '_curl_response', $rest);
+        $this->assertAttributeEmpty( 'curlResponse', $rest);
         $rest->GET();
-        $this->assertAttributeNotEmpty( '_curl_response', $rest);
+        $this->assertAttributeNotEmpty( 'curlResponse', $rest);
         $result = $rest->getCurlResponse();
-        $this->assertAttributeEquals( $result, '_curl_response', $rest);
+        $this->assertAttributeEquals( $result, 'curlResponse', $rest);
     }
 
     public function testPOST() {
         $rest = new \alphayax\utils\Rest( 'http://jsonplaceholder.typicode.com/posts');
         $rest->addHeader( 'User-Agent', 'alphayax-php_utils');
-        $this->assertAttributeEmpty( '_curl_response', $rest);
+        $this->assertAttributeEmpty( 'curlResponse', $rest);
         $rest->POST([
             'userId' => 1,
             'title'  => 'fooBar',
             'body'  => 'toto',
         ]);
-        $this->assertAttributeNotEmpty( '_curl_response', $rest);
+        $this->assertAttributeNotEmpty( 'curlResponse', $rest);
     }
 
     public function testPUT() {
         $rest = new \alphayax\utils\Rest( 'http://jsonplaceholder.typicode.com/posts/1');
         $rest->addHeader( 'User-Agent', 'alphayax-php_utils');
-        $this->assertAttributeEmpty( '_curl_response', $rest);
+        $this->assertAttributeEmpty( 'curlResponse', $rest);
         $rest->PUT([
             'id' => 1,
             'title' => 'toto',
         ]);
-        $this->assertAttributeNotEmpty( '_curl_response', $rest);
+        $this->assertAttributeNotEmpty( 'curlResponse', $rest);
     }
 
     public function testDELETE() {
