f424ff97e095a571c9d0764ad3c40303c46227ac
alphayax <alphayax@gmail.com>
2016-06-22 13:28:29 +0200

    Update project structure Add documentation Update composer.json

--- example/FileSystem.php
+++ example/FileSystem.php
@@ -2,7 +2,7 @@
 
 require_once '../vendor/autoload.php';
 
-$Dir = new \alphayax\utils\file_system\Directory( '../utils');
+$Dir = new \alphayax\utils\file_system\Directory( '../src');
 $Files = $Dir->getFilesByExtension( 'php', true);
 
 print_r( $Files);
